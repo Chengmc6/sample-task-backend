77c143870dfb3d7029679c85c37064f6dab79a2f
GAOMING <gaomingc6@gmail.com>
2025-10-16 01:20:01 +0900

    添加了登录时用户名的判断逻辑的注释

--- src/main/java/com/example/task/common/CustomerUserDetailsService.java
+++ src/main/java/com/example/task/common/CustomerUserDetailsService.java
@@ -11,12 +11,18 @@ import org.springframework.stereotype.Service;
 import com.example.task.persistence.entity.User;
 import com.example.task.persistence.mapper.UserMapper;
 
+/**
+ * 自定义的UserDetailsService实现类，用来判断用户名是否存在
+ * @author gaoming
+ * @since  2025-10-11
+ */
 @Service
 public class CustomerUserDetailsService implements UserDetailsService{
 
     @Autowired
     private UserMapper userMapper;
 
+    //根据查询获取用户信息，保存到CustomerUserDetails里，包括id，username，password等
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user=userMapper.selectByUsername(username);
